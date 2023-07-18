d53e8c557d06d130e865c43ccf9d723e0dc2e9d0
devHanif <devhanif.main@gmail.com>
2023-07-18 11:01:04 +0800

    3

---
+++ public/assets/index.ts
@@ -0,0 +1,8 @@
+import logo from "./images/devHanif.png";
+import profileImg from "./images/profileImg.jpg";
+import wave from "./images/wave.gif";
+import Passw from "./images/1.jpg";
+import DClock from "./images/2.jpg";
+import PortF from "./images/3.jpg";
+
+export { logo, profileImg, wave, Passw, DClock, PortF };
